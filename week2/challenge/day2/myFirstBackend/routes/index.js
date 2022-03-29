a6f319cac5f7c5ec7c884ba65b1bca77dc00c168
Valentin Vandeuren <valentinvandeuren.code@gmail.com>
2022-03-29 10:48:01 +0200

    Fin chanllenge myFirstBackend

--- week2/challenge/day2/myFirstBackend/routes/index.js
+++ week2/challenge/day2/myFirstBackend/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var firstname = "John";
+var lastname = "Doe";
+var age = 40;
+var animals = 2;
+var born = "New York";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Backend with NodeJS & Express', subtitle: 'Coding Classes @LaCapsule' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Backend with NodeJS & Express', subtitle: '@LaCapsule' });
+});
+
+router.get('/userinfo', function(req, res, next) {
+  res.render('userinfo', { title: 'Backend with NodeJS & Express', subtitle: '@LaCapsule', prenom: firstname, nom: lastname, age: age, animals: animals, naissance: born});
 });
 
 module.exports = router;
